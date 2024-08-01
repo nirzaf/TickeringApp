4af9ef88076a08b963c23a9398a0c23f9374cd08
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-08-01 12:54:56 +0300

    Creates an `Employee` class and instantiates it with user input.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter Your Name: ");
+
+var name = Console.ReadLine();
+
+Console.WriteLine("Enter Your Position: ");
+
+var position = Console.ReadLine();
+
+Console.WriteLine("Enter Your Salary: ");
+
+var salary = Convert.ToInt32(Console.ReadLine());
+
+var employee = new Employee(name, position, salary);
+
+Console.WriteLine($"Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}");
+
+public class Employee(string name, string position, int salary)
+{
+    public string Name { get; set; } = name;
+    public string Position { get; set; } = position;
+    public float Salary { get; set; } = salary;
+}
