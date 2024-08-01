c65cc6c4f281cfe84ac2329ef95ef2a2694ac092
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-08-01 14:12:51 +0300

    feat: Implement Benchmarking for List Operations (master)
    This commit introduces benchmarking to compare the performance of
    using `Contains()` vs `Any()` for searching an element in a list.
    
    The `BenchmarkDotNet` library is used to measure the execution time
    of both methods.
    
    This benchmarking will help in identifying the most efficient
    approach for searching elements in lists within the application.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Enter Your Name: ");
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-var name = Console.ReadLine();
+namespace TickeringApp;
 
-Console.WriteLine("Enter Your Position: ");
-
-var position = Console.ReadLine();
-
-Console.WriteLine("Enter Your Salary: ");
-
-var salary = Convert.ToInt32(Console.ReadLine());
-
-var employee = new Employee(name, position, salary);
-
-Console.WriteLine($"Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}");
-
-public class Employee(string name, string position, int salary)
+internal static class Program
 {
-    public string Name { get; set; } = name;
-    public string Position { get; set; } = position;
-    public float Salary { get; set; } = salary;
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<MyBenchmark>();
+        Console.WriteLine(summary);         
+    }
 }
+
+public class MyBenchmark
+{
+    private readonly List<int> _numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    [Benchmark]
+    public bool MeasureUsingContains() => _numbers.Contains(5);
+
+    [Benchmark]
+    public bool MeasureUsingAny() => _numbers.Any(x => x == 5);
+}
+
+
+
+// Console.WriteLine("Enter Your Name: ");
+//
+// var name = Console.ReadLine();
+//
+// Console.WriteLine("Enter Your Position: ");
+//
+// var position = Console.ReadLine();
+//
+// Console.WriteLine("Enter Your Salary: ");
+//
+// var salary = Convert.ToInt32(Console.ReadLine());
+//
+// var employee = new Employee(name, position, salary);
+//
+// Console.WriteLine($"Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}");
+//
+// public class Employee(string name, string position, int salary)
+// {
+//     public string Name { get; set; } = name;
+//     public string Position { get; set; } = position;
+//     public float Salary { get; set; } = salary;
+// }
