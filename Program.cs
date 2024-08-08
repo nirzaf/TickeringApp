3b9b82f0ed8a6023efe0a89f0e836863c51ab7e6
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-08-08 23:18:12 +0300

    feat: implement cpu usage tracker (master)
    Implement a CPU usage tracker to monitor and display the current CPU
    utilization.
    
    This feature provides insights into CPU performance and helps identify
    potential bottlenecks.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-
-using System.Diagnostics;
-using System;
 using System.Management;
+using TicketingApp;
 
-namespace TicketingApp;
+namespace TickeringApp;
 
 static class Program
 {
@@ -36,30 +32,3 @@ static class Program
         Console.WriteLine($"CPU Usage: {cpuUsage}%");
     }
 }
-/*
-internal static class Program
-{
-static void Main(string[] args)
-{
-var summary = BenchmarkRunner.Run<MyBenchmark>();
-Console.WriteLine(summary);
-}
-}
-
-//     | Method               | Mean      | Error     | StdDev    | Median    |
-//     |--------------------- |----------:|----------:|----------:|----------:|
-//     | MeasureUsingContains |  4.020 ns | 0.1777 ns | 0.4983 ns |  3.990 ns |
-//     | MeasureUsingAny      | 24.025 ns | 1.2563 ns | 3.5843 ns | 22.780 ns |
-
-
-public class MyBenchmark
-{
-private readonly List<int> _numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-[Benchmark]
-public bool MeasureUsingContains() => _numbers.Contains(5);
-
-[Benchmark]
-public bool MeasureUsingAny() => _numbers.Any(x => x == 5);
-}
-*/
